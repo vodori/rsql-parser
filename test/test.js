712ce626f9a707a39320c51e19d5e9d2d1f03d9d
paul.rutledge <paul.rutledge@vodori.com>
2018-02-02 10:57:41 -0600

    add initial parsing test and travis ci

--- test/test.js
+++ test/test.js
@@ -1,8 +1,27 @@
 var assert = require('assert');
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal([1,2,3].indexOf(4), -1);
+var antlr4 = require('antlr4');
+var Lexer = require('../gen/RSQLLexer');
+var Parser = require('../gen/RSQLParser');
+var Visitor = require('../gen/RSQLVisitor');
+
+
+describe('Parsing', function () {
+
+    describe('basic equality expression', function () {
+        it('should parse into a comparison node.', function () {
+
+            var expression = "things=='stuff'";
+            var characters = new antlr4.InputStream(expression);
+            var lexer = new Lexer.RSQLLexer(characters);
+            var tokens = new antlr4.CommonTokenStream(lexer);
+            var parser = new Parser.RSQLParser(tokens);
+
+            parser.buildParseTrees = true;
+            var tree = parser.statement();
+            console.log(tree);
+
         });
     });
+
+
 });
